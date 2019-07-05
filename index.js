55c3df9195266b4c2ee261a14b57ef756759e992
Cristian de Mellos <cristian.ms@hotmail.com>
2019-07-04 22:10:33 -0300

    Exercícios com express (Nodejs)

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+//app.use ou app.all = todos tipo de requisicao
+app.post('/teste1', (req, res) => {
+    //res.send('Estou bem...') //envia resposta texto
+    
+        // res.json({
+        //     "nome": "Cristian",
+        //     "idade": 32
+        // }) //envia resposta json
+
+        res.json([
+            {"nome": "Cristian", "idade": 32},
+            {"nome": "Bruna", "idade": 25},
+        ]) //envia resposta json
+})
+
+app.listen(3000, () => {
+    console.log('Backend executando...')
+})
