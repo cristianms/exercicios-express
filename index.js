1ff350db135cd1f712f5c231d68bed0935bc9111
Cristian de Mellos <cristian.ms@hotmail.com>
2019-07-04 23:05:27 -0300

    Testes com diferente tipos de requisição, post, get, etc

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 const express = require('express')
 const app = express()
 
-//app.use ou app.all = todos tipo de requisicao
-app.post('/teste1', (req, res) => {
-    //res.send('Estou bem...') //envia resposta texto
-    
-        // res.json({
-        //     "nome": "Cristian",
-        //     "idade": 32
-        // }) //envia resposta json
-        
+const saudacao = require('./saudacaoMid')
 
-        res.json([
-            {"nome": "Cristian", "idade": 32},
-            {"nome": "Bruna", "idade": 25},
-        ]) //envia resposta json
+app.use(saudacao('Cristian'))
+
+app.use((req, res, next) => {
+    console.log('Antes...')
+    next()
 })
 
+app.get('/clientes/relatorio', (req, res) => {
+    res.send(`Relatório cliente: completo ${req.query.completo} ano ${req.query.ano}`)
+})
+
+app.post('/corpo', (req, res) => {
+    let corpo = ''
+    req.on('data', function(parte) {
+        corpo += parte
+    })
+    req.on('end', function() {
+        res.send(corpo)
+    })
+})
+
+app.get('/clientes/:id', (req, res) => {
+    res.send(`Cliente ${req.params.id} selecionado`)
+})
+
+//app.use ou app.all = todos tipo de requisicao
+app.get('/opa', (req, res, next) => {
+    console.log('Durante...')
+    //res.send('Estou bem...') //envia resposta texto
+    // res.json({
+    //     "nome": "Cristian",
+    //     "idade": 32
+    // }) //envia resposta json
+    res.json([
+        {"nome": "Cristian", "idade": 32},
+        {"nome": "Bruna", "idade": 25},
+    ]) //envia resposta json
+
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('Depois...')
+    next()
+})
+    
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
